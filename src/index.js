608d85f0b933280e9049fea78850dc551234af94
Fei Liu <liu.feiwood@gmail.com>
2020-01-31 07:27:07 +0100

    Add china map

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import React, { Component } from "react";
-import { View } from "react-native";
-import PropTypes from "prop-types";
-import { WebView } from "react-native-webview";
+import React, { Component } from 'react'
+import { View } from 'react-native'
+import PropTypes from 'prop-types'
+import { WebView } from 'react-native-webview'
+import chinaJson from './chinaJson'
 
-import { getMinifiedEChartsFramework } from "./chartconfig";
-import * as jsBuilder from "./jsBuilder";
+import { getMinifiedEChartsFramework } from './chartconfig'
+import * as jsBuilder from './jsBuilder'
 
 class ECharts extends Component {
   static propTypes = {
@@ -13,21 +14,21 @@ class ECharts extends Component {
     canvas: PropTypes.bool,
     onLoadEnd: PropTypes.func,
     backgroundColor: PropTypes.string,
-    customTemplatePath: PropTypes.string
-  };
+    customTemplatePath: PropTypes.string,
+  }
 
   static defaultProps = {
     onData: () => {},
     legacyMode: false,
     canvas: false,
     onLoadEnd: () => {},
-    backgroundColor: "rgba(0, 0, 0, 0)"
-  };
+    backgroundColor: 'rgba(0, 0, 0, 0)',
+  }
 
   constructor(props) {
-    super(props);
-    this.onGetHeight = null;
-    this.callbacks = {};
+    super(props)
+    this.onGetHeight = null
+    this.callbacks = {}
 
     this.html = `
       <!DOCTYPE html>
@@ -58,109 +59,113 @@ class ECharts extends Component {
         <body>
             <div id="main">
             </div>
+
+            <script>
+              echarts.registerMap('china', ${JSON.stringify(chinaJson)});
+            </script>
         </body>
 
-      </html>`;
+      </html>`
   }
 
   onMessage = e => {
     try {
-      if (!e) return null;
+      if (!e) return null
 
-      const { onData } = this.props;
+      const { onData } = this.props
 
-      const data = JSON.parse(unescape(unescape(e.nativeEvent.data)));
+      const data = JSON.parse(unescape(unescape(e.nativeEvent.data)))
 
-      if (data.types === "DATA") {
-        onData(data.payload);
-      } else if (data.types === "CALLBACK") {
+      if (data.types === 'DATA') {
+        onData(data.payload)
+      } else if (data.types === 'CALLBACK') {
         /* eslint-disable no-case-declarations */
-        const { uuid } = data;
+        const { uuid } = data
         /* eslint-enable no-case-declarations */
-        this.callbacks[uuid](data.payload);
+        this.callbacks[uuid](data.payload)
       }
     } catch (error) {
-      console.log(error);
+      console.log(error)
     }
-  };
+  }
 
   postMessage = data => {
-    this.webview.postMessage(jsBuilder.convertToPostMessageString(data));
-  };
+    this.webview.postMessage(jsBuilder.convertToPostMessageString(data))
+  }
 
   ID = () =>
     `_${Math.random()
       .toString(36)
-      .substr(2, 9)}`;
+      .substr(2, 9)}`
 
   setBackgroundColor = color => {
     const data = {
-      types: "SET_BACKGROUND_COLOR",
-      color
-    };
-    this.postMessage(data);
-  };
+      types: 'SET_BACKGROUND_COLOR',
+      color,
+    }
+    this.postMessage(data)
+  }
 
   getOption = (callback, properties = undefined) => {
-    const uuid = this.ID();
-    this.callbacks[uuid] = callback;
+    const uuid = this.ID()
+    this.callbacks[uuid] = callback
     const data = {
-      types: "GET_OPTION",
+      types: 'GET_OPTION',
       uuid,
-      properties
-    };
-    this.postMessage(data);
-  };
+      properties,
+    }
+    this.postMessage(data)
+  }
 
   setOption = (option, notMerge, lazyUpdate) => {
     const data = {
-      types: "SET_OPTION",
+      types: 'SET_OPTION',
       payload: {
         option,
         notMerge: notMerge || false,
-        lazyUpdate: lazyUpdate || false
-      }
-    };
-    this.postMessage(data);
-  };
+        lazyUpdate: lazyUpdate || false,
+      },
+    }
+    this.postMessage(data)
+  }
 
   clear = () => {
     const data = {
-      types: "CLEAR"
-    };
-    this.postMessage(data);
-  };
+      types: 'CLEAR',
+    }
+    this.postMessage(data)
+  }
 
   getWebViewRef = ref => {
-    this.webview = ref;
-  };
+    this.webview = ref
+  }
 
   onLoadEnd = () => {
     if (this.webview) {
-      this.webview.injectJavaScript(jsBuilder.getJavascriptSource(this.props));
+      this.webview.injectJavaScript(jsBuilder.getJavascriptSource(this.props))
     }
-    this.props.onLoadEnd();
-  };
+    this.props.onLoadEnd()
+  }
 
   render() {
-    let source = {};
+    let source = {}
 
     if (this.props.customTemplatePath) {
       source = {
-        uri: this.props.customTemplatePath
-      };
+        uri: this.props.customTemplatePath,
+      }
     } else {
       source = {
         html: this.html,
-        baseUrl: ""
-      };
+        baseUrl: '',
+      }
     }
 
     return (
       <View style={{ flex: 1 }}>
         <WebView
           ref={this.getWebViewRef}
-          originWhitelist={["*"]}
+          originWhitelist={['*']}
           scrollEnabled={false}
           source={source}
           onMessage={this.onMessage}
@@ -170,8 +175,8 @@ class ECharts extends Component {
           onLoadEnd={this.onLoadEnd}
         />
       </View>
-    );
+    )
   }
 }
 
-export { ECharts };
+export { ECharts }
